18895f2c1d209d45280c3d13ad83ea5218443b57
Erick Awino <erick.otieno89@gmail.com>
2026-03-30 13:08:20 +0300

    feat(core): add central url reconciliation function

---
+++ backend/core/urls/__init__.py
@@ -0,0 +1,19 @@
+from .frontend_url import (
+    build_frontend_url_for_user,
+    get_frontend_base_url_for_user,
+    get_login_url,
+    get_profile_edit_url,
+    get_profile_url,
+    get_security_settings_url,
+    get_settings_url,
+)
+
+__all__ = [
+    "build_frontend_url_for_user",
+    "get_frontend_base_url_for_user",
+    "get_login_url",
+    "get_profile_edit_url",
+    "get_profile_url",
+    "get_security_settings_url",
+    "get_settings_url",
+]
